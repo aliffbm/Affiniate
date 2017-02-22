ab02f9b8885405b7926f190c109f06820c8c9817
aliff.macapinlac <aliff.macapinlac@gmail.com>
2017-02-21 22:13:34 -0800

    drag in drop is getting theregit add .!

--- src/pages/chores.js
+++ src/pages/chores.js
@@ -19,23 +19,25 @@ for(var i=0;i<Data[1].chores.length;i++){
 
 
 
-const SortableItem = SortableElement(({value}) => <div>{value}</div>);
+const SortableItem = SortableElement(({value, value2, value3}) =><div>{value2} <img className="text-center" style={{width: "500px"}} id="listImages" src={value}></img>{value3}</div>);
 
 const SortableList = SortableContainer(({items}) => {
     return (
-        <div className="container text-center" id="listContainer">
+        <div className="container">
           <ul>
             {items.map((value, index) =>
-                <SortableItem key={`item-${index}`} index={index} value={value} />
+              <div className="container">
+                <SortableItem key={`item-${index}`} index={index} value={value.image} value2={value.name} value3={index + 1}/>
+              </div>
             )}
           </ul>
         </div>
     );
 });
 
-export default class choresP extends Component {
+export default class chores extends Component {
     state = {
-        items: [choreItems[0].name, choreItems[1].name, choreItems[2].name, choreItems[3].name]
+        items: [choreItems[0], choreItems[1], choreItems[2], choreItems[3]]
     }
     onSortEnd = ({oldIndex, newIndex}) => {
         this.setState({
@@ -44,7 +46,9 @@ export default class choresP extends Component {
     };
     render() {
         return (
+          <div className="container text-center" id="listContainer">
             <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+          </div>
         )
     }
 }
@@ -86,7 +90,7 @@ export default class Chores extends React.Component {
        backgroundImage: 'url(' + chore.image + ')',
       };
       
-      
+      }
       return(<div></div>);
 
     })
